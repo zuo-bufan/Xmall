66a1f291b148cc1dd83dd384b38ff1d0fe053f8c
zuobufan <2605331201@qq.com>
2021-12-01 16:33:44 +0800

    打包

--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-// 正则校验手机号
-export function validateTelephone(value) {
-  let reg = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
-  return reg.test(value.trim());
-}
